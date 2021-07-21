eb5be54eaffbf6f31f757146012413693d7a7b9c
Lewicv83 <lewicv83@gmail.com>
2021-07-21 13:35:05 -0400

    Initial commit

---
+++ Animal Combat/Animal Combat/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Animal_Combat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
